50013aaaf3a6c825cae3535ffded0775acba10a1
Paul Melnikow <email@paulmelnikow.com>
2019-08-24 09:26:13 -0400

    Reword and remove more indirection

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
   removeInterceptor,
   disableNetConnect,
   enableNetConnect,
-  removeAll: cleanAll,
+  removeAll,
 } = require('./lib/intercept')
 const recorder = require('./lib/recorder')
 const { Scope, load, loadDefs, define } = require('./lib/scope')
@@ -27,11 +27,12 @@ Object.assign(module.exports, {
   removeInterceptor,
   disableNetConnect,
   enableNetConnect,
-  // TODO-12.x Historically cleanAll() has returned the nock global. It's
-  // not clear this was deliberate or is even helpful. This shim is
-  // included for backward compatibility.
+  // TODO-12.x Historically `nock.cleanAll()` has returned the nock global.
+  // The other global methods do noto do this, so it's not clear this was
+  // deliberate or is even helpful. This shim is included for backward
+  // compatibility and shoulud be replaced with an alias to `removeAll()`.
   cleanAll() {
-    cleanAll()
+    removeAll()
     return module.exports
   },
   load,
